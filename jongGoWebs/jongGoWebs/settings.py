e8bd773865b3bef8a331c1d8c32fc287204deb8f
dk <kamal1234@naver.com>
2021-06-27 11:27:39 +0900

    06_27

--- jongGoWebs/jongGoWebs/settings.py
+++ jongGoWebs/jongGoWebs/settings.py
@@ -89,11 +89,14 @@ WSGI_APPLICATION = 'jongGoWebs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jongGo',
+        'USER': 'jongGo_role',
+        'PASSWORD': '1234567890',
+        'HOST': '127.0.0.1',
+        'PORT':'5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
