cfac457789af9756869af99199e8882ed12b5a72
dk <kamal1234@naver.com>
2021-06-27 19:39:33 +0900

    06_27(DB 연동 후 HTML 띄우기)

--- jongGoWebs/jongGoApp/models.py
+++ jongGoWebs/jongGoApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class ProductDetail(models.Model):
+    product_idx = models.IntegerField(db_column='PRODUCT_IDX', primary_key=True)  # Field name made lowercase.
+    user_id = models.CharField(db_column='USER_ID', max_length=30)  # Field name made lowercase.
+    product_name = models.CharField(db_column='PRODUCT_NAME', max_length=30)  # Field name made lowercase.
+    product_code = models.CharField(db_column='PRODUCT_CODE', max_length=30)  # Field name made lowercase.
+    product_reg = models.DateField(db_column='PRODUCT_REG')  # Field name made lowercase.
+    product_edit = models.DateField(db_column='PRODUCT_EDIT')  # Field name made lowercase.
+    product_comment = models.CharField(db_column='PRODUCT_COMMENT', max_length=500, blank=True, null=True)  # Field name made lowercase.
+    product_price = models.IntegerField(db_column='PRODUCT_PRICE')  # Field name made lowercase.
+    product_res = models.CharField(db_column='PRODUCT_RES', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    product_state = models.CharField(db_column='PRODUCT_STATE', max_length=30)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'product_detail'
--- jongGoWebs/jongGoApp/views.py
+++ jongGoWebs/jongGoApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from .models import ProductDetail
 
 # Create your views here.
 
@@ -9,7 +10,8 @@ from firebase_admin import db
 def index(request):
     #dir = db.reference()  # 기본위치 지정
     #data_1 = db.reference().child()
-    return render(request, 'jongGoApp/index.html')
+    posts = ProductDetail.objects.all()
+    return render(request, 'jongGoApp/index.html', {'posts': posts})
 
 def join(request):
     return render(request, 'jongGoApp/join.html')
